8edbaed9c59256d7cb21b56e75c0a843d671fde2
Ricardo Fabbri <rfabbri@lems.brown.edu>
2015-10-02 17:52:43 -0300

    simple script to list running blenders on node

--- misc/Client.py
+++ misc/Client.py
@@ -1,6 +1,7 @@
 import bpy
 bpy.data.screens["Default"].name = "Default"
-bpy.data.window_managers["WinMan"].addon_filter = 'Render'
+#bpy.data.window_managers["WinMan"].addon_filter = 'Render'
+bpy.ops.wm.addon_enable(module ='netrender')
 bpy.context.scene.render.engine = 'NET_RENDER'
 bpy.context.scene.network_render.mode = 'RENDER_CLIENT'
 bpy.context.scene.network_render.server_address = "192.168.1.116" #cluster"10.0.10.100"
