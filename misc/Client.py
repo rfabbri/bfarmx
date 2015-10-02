068dbf065d11b65f6fe59538b74cfb8385a3c50d
Ricardo Fabbri <rfabbri@lems.brown.edu>
2015-10-02 17:48:58 -0300

    script to startup a client, possibly obsolete

---
+++ misc/Client.py
@@ -0,0 +1,11 @@
+import bpy
+bpy.data.screens["Default"].name = "Default"
+bpy.data.window_managers["WinMan"].addon_filter = 'Render'
+bpy.context.scene.render.engine = 'NET_RENDER'
+bpy.context.scene.network_render.mode = 'RENDER_CLIENT'
+bpy.context.scene.network_render.server_address = "192.168.1.116" #cluster"10.0.10.100"
+#bpy.ops.wm.save_mainfile()
+
+bpy.ops.render.netclientanim()
+
+
